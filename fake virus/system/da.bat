8ddb7f68d26c9ebf568d5d3d5c8bb6741a9236a6
zardaxYT5 <146130254+zardaxYT5@users.noreply.github.com>
2023-09-27 18:27:27 +0200

    Create da.bat

---
+++ fake virus/system/da.bat
@@ -0,0 +1,22 @@
+@echo off
+echo DONT CLOSE THE WINDOW
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+shutdown /s /t 50 /c "hi"
+
+timeout 10
+
+shutdown /a
+echo.
